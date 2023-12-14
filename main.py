99c6d73b385e34832aef12c8a31d71629f2f5ba1
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-12-14 16:05:21 +0900

    インデントを追加した

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import json
 import sys
 import webbrowser as wb
 
-def dict_html(struct: dict) -> str:
+def dict_html(struct: dict, indent: int) -> str:
     html_text = ""
     for (tag,value) in struct.items():    
-        html_text += f"<{tag}>"
+        html_text += '\t'*indent + f"<{tag}>\n"
         if isinstance(value, dict):
-            html_text += dict_html(value)
+            html_text += dict_html(value, indent+1)
         else:
-            html_text += value
-        html_text += f"</{tag}>"
+            html_text += '\t'*indent + f"\t{value}\n"
+        html_text += '\t'*indent + f"</{tag}>\n"
     return html_text
 
 name = sys.argv[1]
 file = open(name, mode="r", encoding="utf-8")
 struct: dict = json.load(file)
-html = dict_html(struct)
+html = dict_html(struct, 0)
 
 name = f"{name.split('.')[0]}.html"
 open(name, mode="w", encoding="utf-8").write(html)
