0c53d9a2da1d46864fb8c7142c7a09356d9304a4
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-12-15 17:29:38 +0900

    コメントとエラー処理を追加した

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import json
 import argparse as ap
 import webbrowser as wb
 
+# コマンドライン引数の設定
 parser = ap.ArgumentParser(description='jsonデータからHTMLを作成するツール')
 parser.add_argument('input_file', help='入力するjsonファイル')
 parser.add_argument('-o', '--open', action='store_true', help='作成したHTMLをブラウザで開きます')
 args = parser.parse_args()
 
-
 def dict_html(struct: dict, indent: int) -> str:
+    '''
+    辞書データをHTMLテキストに変換します。    
+    '''
+
     html_text = ""
     for (tag,value) in struct.items():    
         html_text += '\t'*indent + f"<{tag}>\n"
         if isinstance(value, dict):
+            # 再帰的に呼び出す
             html_text += dict_html(value, indent+1)
         else:
             html_text += '\t'*indent + f"\t{value}\n"
@@ -21,19 +26,40 @@ def dict_html(struct: dict, indent: int) -> str:
     return html_text
 
 
-def change_extension(file_path, new_extension):
-    base_path, old_extension = os.path.splitext(file_path)
+def change_extension(file_path: str, new_extension: str):
+    '''
+    ファイルの拡張子を変更します
+    '''
+    base_path, _ = os.path.splitext(file_path)
     new_file_path = base_path + new_extension    
     return new_file_path
 
-path: str = args.input_file
-file = open(path, mode="r", encoding="utf-8")
-struct: dict = json.load(file)
-html = dict_html(struct, 0)
+def main():
+    path: str = args.input_file
 
-new_path = change_extension(path, ".html")
-open(new_path, mode="w", encoding="utf-8").write(html)
+    # ファイルの存在確認
+    if not os.path.exists(path):
+        print("指定されたファイルは存在しません。")
+        return
+    
+    # ファイルがJSON形式かどうか確認
+    try:
+        with open(path, mode="r", encoding="utf-8") as file:
+            struct: dict = json.load(file)
+    except json.JSONDecodeError as e:
+        print("JSON形式のファイルではありません。")
+        return
+    
+    html = dict_html(struct, 0)
 
-is_open = args.open
-if is_open:
-    wb.open(new_path)
+    new_path = change_extension(path, ".html")
+    with open(new_path, mode="w", encoding="utf-8") as html_file:
+        html_file.write(html)
+
+    is_open = args.open
+    if is_open:
+        wb.open(new_path)
+
+
+if __name__ == "__main__":
+    main()
